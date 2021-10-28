fac418418b63a032ba886a174d6b7fcc90e29e7d
Juan Carlos <86580442+JuanLuky@users.noreply.github.com>
2021-10-28 08:06:56 -0300

    updates

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+
+var i = setInterval(function () {
+    
+    clearInterval(i);
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("container").style.display = "inline";
+
+}, 2410);
